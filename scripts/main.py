a0140e293f062bb476f409ed6e4c1ace6fac25ab
foilhatguy <antony.aleynikov@gmail.com>
2019-05-05 19:00:25 +0300

    two classes now

--- scripts/main.py
+++ scripts/main.py
@@ -1,24 +1,42 @@
 import tkinter as tk
 import tkinter.filedialog as fd
 import tkinter.ttk as ttk
+
 import pandas as pd
 
 
-class Interface:
+class Data:
     def __init__(self):
-        self.df = pd.read_csv("../data/weather.csv", encoding="utf-8", sep=";")
+        self.dataframe = pd.read_csv("../data/weather.csv", encoding="utf-8", sep=";")
+        # self.getdata(self.dataframe)
+
+    def getdata(self, filters):
+        if filters == 'all':
+            return self.dataframe
+
+
+class Gui:
+    def __init__(self, data):
+        self.pointer = data
+        self.df = self.askdata()
         root = tk.Tk()
-        root.resizable(False, False)
+        # root.resizable(False, False)
+
+        top = ttk.Frame(root, relief='groove', borderwidth=5)
+        top.pack(anchor='n')
+
+        top_left = ttk.Frame(top, relief='groove', borderwidth=5)
+        top_left.grid(column=0, row=0)
         # upper toolbar with filters
-        toolbar = ttk.Frame(root)
-        toolbar.grid(column=0, row=0, columnspan=3)
+        toolbar = ttk.Frame(top_left, relief='groove', borderwidth=5)
+        toolbar.grid(row=0, column=0, columnspan=3)
         button1 = ttk.Button(toolbar, text="Insert row", command=lambda: self.insert(self.df))
         button1.grid(row=0, column=0, pady=8)
         # toolbar ends
 
         # table starts
-        tableframe = ttk.Frame(root)
-        tableframe.grid(column=0, row=1, columnspan=3)
+        tableframe = ttk.Frame(top_left, relief='groove', borderwidth=5)
+        tableframe.grid(row=1, column=0, columnspan=3)
 
         self.table = ttk.Treeview(tableframe, columns=list(range(7)))
 
@@ -29,7 +47,7 @@ class Interface:
         self.table.pack(side='left', fill='y')
 
         self.table.column(column="#0", width=50)
-        self.table.heading(column="#0", text='index')
+        self.table.heading(column="#0", text='ID')
 
         self.table.column(column=0, width=190)
         self.table.heading(column=0, text='city')
@@ -54,7 +72,7 @@ class Interface:
         # table ends
 
         # right editor panel
-        editor = ttk.Frame(root)
+        editor = ttk.Frame(top, relief='groove', borderwidth=5)
         editor.grid(column=1, row=0, columnspan=3)
         button1 = ttk.Button(editor, text="Insert row", command=lambda: self.insert(self.df))
         button1.grid(row=0, column=0, pady=8)
@@ -66,8 +84,20 @@ class Interface:
         # editor panel ends
         self.view(self.df)
 
-        root.update()
+        # canvas with graphs
+        bottom = ttk.Frame(root, relief='groove', borderwidth=5, height=100, width=100)
+        bottom.pack(anchor='s', fill='y')
 
+        button1 = ttk.Button(bottom, text="In1sert row", command=lambda: self.insert(self.df))
+        button1.grid(row=0, column=0, pady=8)
+        button2 = ttk.Button(bottom, text="De1lete row", command=lambda: self.delete(self.df))
+        button2.grid(row=1, column=0, pady=8)
+
+        button2 = ttk.Button(bottom, text="Sa1ve to disk", command=lambda: self.save(self.df))
+        button2.grid(row=2, column=0, pady=8)
+        # /canvas
+
+        root.update()
         root.mainloop()
 
     @staticmethod
@@ -82,7 +112,7 @@ class Interface:
 
     def delete(self, data):
         pass
-    
+
     def view(self, data):
         for row in data.iterrows():
             rowdata = row[1].tolist()
@@ -90,6 +120,10 @@ class Interface:
         # example of element:
         print(list(data.iterrows())[0])
 
+    def askdata(self):
+        filters = 'all'
+        return self.pointer.getdata(filters)
+
 
 if __name__ == "__main__":
-    interface = Interface()
+    Gui(Data())
